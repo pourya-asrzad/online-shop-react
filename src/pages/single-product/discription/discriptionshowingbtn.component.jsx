a481c786ffe60e22c04c29e87e14c3c78b0c92ba
Pouria Asrzad <poriaasrzad@gmail.com>
2023-01-13 18:22:59 +0330

    handel discription more & less

--- src/pages/single-product/discription/discriptionshowingbtn.component.jsx
+++ src/pages/single-product/discription/discriptionshowingbtn.component.jsx
@@ -5,7 +5,7 @@ import Styles from './discription.module.scss'
 const Discriptionshowingbtn = ({ onClick, children }) => {
     return (
         <div className={Styles.buttonparent}>
-            <IoIosArrowBack style={{ color: 'blue' }} />
+            <IoIosArrowBack style={{ color: '#2173c0' }} />
             <button onClick={onClick}>{children}</button>
         </div>
     );
--- src/pages/single-product/image-slider/ImageSlider.component.jsx
+++ src/pages/single-product/image-slider/ImageSlider.component.jsx
@@ -5,23 +5,13 @@ const ImageSlider = () => {
     return (
         <>
             <Carousel variant="dark" className={Styles.Carousel}>
+
                 <Carousel.Item>
                     <div className={Styles.imagecontainer}>
 
                         <img
                             className="d-block w-100"
-                            src="https://dkstatics-public.digikala.com/digikala-products/3b085990688cff94161ba2f66559d140af1e37d0_1631100591.jpg?x-oss-process=image/resize,m_lfit,h_300,w_300/quality,q_80"
-                            alt="First slide"
-                        />
-                    </div>
-
-                </Carousel.Item>
-                <Carousel.Item>
-                    <div className={Styles.imagecontainer}>
-
-                        <img
-                            className="d-block w-100"
-                            src="https://dkstatics-public.digikala.com/digikala-products/120669435.jpg?x-oss-process=image/resize,m_lfit,h_300,w_300/quality,q_80"
+                            src="https://dkstatics-public.digikala.com/digikala-products/a9ab05d6c45423bbcebaa6e4e3efe0126de08cc4_1670074734.jpg?x-oss-process=image/resize,m_lfit,h_800,w_800/quality,q_90"
                             alt="Second slide"
                         />
                     </div>
@@ -29,8 +19,8 @@ const ImageSlider = () => {
                 <Carousel.Item>
                     <div className={Styles.imagecontainer}>
                         <img
-                            className="d-block "
-                            src="https://dkstatics-public.digikala.com/digikala-products/3665339.jpg?x-oss-process=image/resize,m_lfit,h_300,w_300/quality,q_80"
+                            className="d-block w-100 "
+                            src="https://dkstatics-public.digikala.com/digikala-products/6e25f9b57c920efd55f980d129614b18f83bd451_1670074732.jpg?x-oss-process=image/resize,m_lfit,h_800,w_800/quality,q_90"
                             alt="Third slide"
                         />
                     </div>
