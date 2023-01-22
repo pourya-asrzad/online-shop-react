e74a4980dc63104fb10b82913018f1887a71cc20
Pouria Asrzad <poriaasrzad@gmail.com>
2023-01-22 09:30:57 +0330

    inventory price card added

--- src/components/inventory-price-card/InventoryPriceCard.component.jsx
+++ src/components/inventory-price-card/InventoryPriceCard.component.jsx
@@ -1,33 +1,30 @@
 import React from 'react';
 import EditProduct from './EditProduct.component';
 import TitleTextGroup from './TitleTextGroup.component';
-
+import Styles from './InventoryPriceCard.module.scss'
 const InventoryPriceCard = ({ img, title, price, inventory }) => {
     return (
-        <div>
-            <div>
-                <div>
-                    <img src={img} alt={title} />
-                </div>
-                <div>
-                    <div>
-                        <h5>
-                            :نام کالا
-                        </h5>
-                        <span>{title}</span>
-                    </div>
-                    <div>
+        <div className={Styles.inventCard} >
+            <EditProduct price={price} inventory={inventory}></EditProduct>
+            <div className={Styles.cardimageside}>
+                <div className={Styles.inventorydetailparent}>
+                    <TitleTextGroup title={":نام کالا"}>
+                        {title}
+                    </TitleTextGroup>
+                    <div className={Styles.invenbuttom}>
                         <TitleTextGroup title={":قیمت"}>
                             {price}
                         </TitleTextGroup>
-                        <div></div>
+                        <div className='height-hr'></div>
                         <TitleTextGroup title={":موجودی"}>
                             {inventory}
                         </TitleTextGroup>
                     </div>
                 </div>
+                <div className={Styles.imageContainer}>
+                    <img src={img} alt={title} />
+                </div>
             </div>
-            <EditProduct price={price} inventory={inventory}></EditProduct>
         </div>
     );
 }
