8783d84e2d55ecf8a9b1d5f19ec970ef61a5f767
Pouria Asrzad <poriaasrzad@gmail.com>
2023-01-22 10:33:06 +0330

    orders ui added

---
+++ src/components/tabel/Tr.component.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import Button from 'react-bootstrap/Button';
+
+const TrComponent = () => {
+    return (
+        <tr>
+            <td>
+                <Button variant="primary">بررسی سفارش</Button>
+            </td>
+            <td>1401/2/8</td>
+            <td>10000</td>
+            <td>اکبر زمانی</td>
+        </tr>
+    );
+}
+
+export default TrComponent;
--- src/pages/control-panel/orders/orders.page.jsx
+++ src/pages/control-panel/orders/orders.page.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Helmet } from 'react-helmet';
 import PanelTopTitle from '../../../components/panel-top-title/PanelTopTitle.component';
+import TabelComponent from '../../../components/tabel/Tabel.component';
 import { getAppTitle } from '../../../utils/functions.utils';
 import Styles from './orders.module.scss'
 const Orders = () => {
@@ -12,12 +13,24 @@ const Orders = () => {
                 <title>   پنل مدیریت {appTittle} |  سفاارش ها </title>
             </Helmet>
             <div className={Styles.ordersheader}>
+                <div className={Styles.radiosparent}>
+                    <div style={{ marginRight: "29px" }} className={Styles.inputscontainer}>
+                        <label htmlFor="waiting">سفارش های در حال انتظار </label>
+                        <input checked={true} type="radio" name="order" id="waiting" />
+                    </div>
+                    <div className={Styles.inputscontainer}>
+                        <label htmlFor="Delivered">سفارش های تحویل شده </label>
+                        <input type="radio" name="order" id="Delivered" />
+                    </div>
+                </div>
                 <PanelTopTitle color={'#00f500'}>
                     مدیریت سفارش ها
                 </PanelTopTitle>
             </div>
             <div style={{ height: '64px' }} id="inventgost"></div>
-
+            <main>
+                <TabelComponent />
+            </main>
         </>
     );
 }
