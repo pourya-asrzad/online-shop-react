7832dd4b82290072f5a95c55287ee5fe22e8a18b
Pouria Asrzad <poriaasrzad@gmail.com>
2023-01-23 16:09:53 +0330

    private route bug fixed

--- src/routes/private.routes.jsx
+++ src/routes/private.routes.jsx
@@ -2,7 +2,7 @@ import { Navigate, Outlet } from 'react-router-dom'
 import { INTERNAL_PATHS } from '../configs/routs.config'
 export const PrivateRoutes = () => {
     if (localStorage.login) {
-        return <Navigate to={INTERNAL_PATHS.HOME} />
+        return <Navigate to={INTERNAL_PATHS.CONTROLPANEL} />
     } else {
         return (
             <Outlet />
