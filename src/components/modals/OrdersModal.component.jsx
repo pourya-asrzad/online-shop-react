7abc85f724b78a5eda014b617cdc11f4c76967d9
Pouria Asrzad <poriaasrzad@gmail.com>
2023-01-29 11:48:53 +0330

    orders modal ui complete

--- src/components/modals/OrdersModal.component.jsx
+++ src/components/modals/OrdersModal.component.jsx
@@ -1,7 +1,9 @@
 import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
 import FormInput from '../form-input/FormInput.component';
+import TitleTextGroup from '../inventory-price-card/TitleTextGroup.component';
 import Styles from './modals.module.scss'
+import OrdersTabel from './OrdersTabel.component';
 
 function OrdersModal(props) {
 
@@ -14,14 +16,31 @@ function OrdersModal(props) {
         >
             <Modal.Header closeButton>
                 <Modal.Title id="contained-modal-title-vcenter">
-                    افزودن کالا
+                    بررسی سفارش
                 </Modal.Title>
             </Modal.Header>
-            <Modal.Body>
+            <Modal.Body className={Styles.ordersmodalbody}>
+                <section className={Styles.ordermaninfo}>
+                    <TitleTextGroup title={':نام'}>
+                        اکبر زمانی
+                    </TitleTextGroup>
+                    <TitleTextGroup title={':آدرس'}>
+                        تهران,خیابان طوس,کوچه عربی
+                    </TitleTextGroup>
+                    <TitleTextGroup title={':زمان تحویل'}>
+                        1400-5-20
+                    </TitleTextGroup>
+                    <TitleTextGroup title={': زمان سفارش'}>
+                        1400-7-31
+                    </TitleTextGroup>
 
+                </section>
+                <section className={Styles.tabelSection}>
+                    <OrdersTabel />
+                </section>
             </Modal.Body>
-            <Modal.Footer>
-                <Button onClick={props.onHide}>افزودن</Button>
+            <Modal.Footer className={Styles.ordermodalfooter}>
+                <Button variant="success" onClick={props.onHide}>تحویل شد</Button>
             </Modal.Footer>
         </Modal>
     );
---
+++ src/components/modals/OrdersTabel.component.jsx
@@ -0,0 +1,57 @@
+import React from 'react';
+import Styles from './modals.module.scss'
+
+const OrdersTabel = () => {
+    return (
+        <table className={Styles.tabel}>
+            <tr>
+                <th>تعداد</th>
+                <th>قیمت </th>
+                <th style={{ display: 'flex' }}>کالا</th>
+            </tr>
+            <tr>
+                <td>1 </td>
+                <td> 100000</td>
+                <td style={{ display: 'flex' }}>روغن سرخ کردنی 12 کیلویی</td>
+            </tr>
+            <tr>
+                <td>1 </td>
+                <td> 100000</td>
+                <td style={{ display: 'flex' }}> غلام اب داد غلام رف غلام امد غلام کجایی   </td>
+            </tr>
+            <tr>
+                <td>1 </td>
+                <td> 100000</td>
+                <td style={{ display: 'flex' }}>غلام اب داد غلام ر غلام اب داد غلام رف غلام امد غلام کجایی   </td>
+            </tr>
+            <tr>
+                <td>1 </td>
+                <td> 100000</td>
+                <td style={{ display: 'flex' }}>غلام اب داد غلام ر غلام اب داد غلام رف غلام امد غلام کجایی   </td>
+            </tr>
+            <tr>
+                <td>1 </td>
+                <td> 100000</td>
+                <td style={{ display: 'flex' }}>غلام اب داد غلام ر غلام اب داد غلام رف غلام امد غلام کجایی   </td>
+            </tr>
+            <tr>
+                <td>1 </td>
+                <td> 100000</td>
+                <td style={{ display: 'flex' }}>غلام اب داد غلام ر غلام اب داد غلام رف غلام امد غلام کجایی   </td>
+            </tr>
+            <tr>
+                <td>1 </td>
+                <td> 100000</td>
+                <td style={{ display: 'flex' }}>غلام اب داد غلام ر غلام اب داد غلام رف غلام امد غلام کجایی   </td>
+            </tr>
+            <tr>
+                <td>1 </td>
+                <td> 100000</td>
+                <td style={{ display: 'flex' }}>غلام اب داد غلام ر غلام اب داد غلام رف غلام امد غلام کجایی   </td>
+            </tr>
+
+        </table>
+    );
+}
+
+export default OrdersTabel;
